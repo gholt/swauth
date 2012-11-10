8a25ebe88fba155cacaa2bb7cb4a4516146cb157
gholt <z-github@brim.net>
2012-11-10 01:11:15 +0000

    Releasing 1.0.6

--- swauth/__init__.py
+++ swauth/__init__.py
@@ -17,7 +17,7 @@ import gettext
 
 
 #: Version information (major, minor, revision[, 'dev']).
-version_info = (1, 0, 6, 'dev')
+version_info = (1, 0, 6)
 #: Version string 'major.minor.revision'.
 version = __version__ = ".".join(map(str, version_info))
 gettext.install('swauth')
